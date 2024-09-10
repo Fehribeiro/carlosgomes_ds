7d229318bf62813d23359a96fe63122581ec4e73
Fernando <fehribeiro074@gmail.com>
2024-09-10 16:46:46 -0300

    Aula revisão - Media de tempo

---
+++ viagem_carro.py
@@ -0,0 +1,6 @@
+print("Media de tempo viagem de Carro")
+
+velocidade_media = int(input("Qual a velocidade media esperada (Em KM/H)?\n"))
+distancia = int(input("Insira a distancia\n"))
+
+print(f"O tempo esperado para a viagem de acordo com as informações concedidas é de {velocidade_media/distancia:.2f} horas!")
